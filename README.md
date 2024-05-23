95332cc46b25c929180ec25597753c43ec5e5936
Manepalli, Sangeeta G <sangeeta.g.manepalli@intel.com>
2024-05-23 13:48:52 -0700

    updates to detect NPU and GPU driver versions

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# cpudetect
-detects intel cpu's and features
+```
+*******************************************************************************
+** INTEL CONFIDENTIAL
+**
+** Copyright (c) 2020-2024 Intel Corporation All Rights Reserved
+**
+** This software and the related documents are Intel copyrighted materials,
+** and your use of them is governed by the express license under which they
+** were provided to you ("License"). Unless the License provides otherwise,
+** you may not use, modify, copy, publish, distribute, disclose or transmit
+** this software or the related documents without Intel's prior written
+** permission.
+**
+** The source code contained or described herein and all documents related to
+** the source code ("Material") are owned by Intel Corporation or its suppliers
+** or licensors.
+**
+** This software and the related documents are provided as is,
+** with no express or implied warranties, other than those that are expressly
+** stated in the License.
+**
+*******************************************************************************
+```
+# Setup
+This document covers the setup, build and runtime process for the cpu detect functionality. Documentation can be found on google drive [link](https://drive.google.com/drive/folders/1FpDT8CSGsZVlNrsqr2wFRHabYg-5eHLc)
+Note: This is the Windows API based information for MTL release
+
+## Step 1: Download the sample code
+Sample code can be cloned from https://github.com/rrwinterton/cpudetect 
+
+
+## Step 2: Import into VS 2022
+Import the C++ files to VC 2022, this will create a new vcxproj file (Note current code also has existing vcxproj file which can be used for VC2019)
+Pls update the project file to include the header files in the include/ folder for compile
+Pls update the project file to include the library files in the lib/ folder for the linking during build
+
+
+## Step 3: Building the code 
+Cmake file is included with the code which can be imported into VS for generating build files. These can also be run from the command prompt
+
+1. Launch a Developer Command Prompt for Visual Studio 2019. Run `cmake -S . -B build` from unzipped folder.
+2. Run`cmake --build build --config <configuration>` where configuration is `Debug` or `Release`
+
+For Visual Studio click on 'Build' option in Toolbar and select 'Release-x64' option to build the code 
+
+## Pre-MTL code runtime flow
+Code starts in main() function, enters ADL or legacy cpudetect code 
+Updated to include memory information and GPU driver version
+
+## MTL/ARL code runtime flow
+Code starts in main() function, MTL/ARL native code detection is executed first
+Updated to include memory information and GPU, NPU driver version
+
+
+## Running CPU detect
+ Execute cpudetect 
+  - `cd build\<configuration>`
+  - `cpudetect.exe` to run the C++ sample application
--- cpudetect.cpp
+++ cpudetect.cpp
@@ -2,17 +2,349 @@
 //
 
 #include <windows.h>
+#include <stdlib.h>
 #include <string>
 #include <iostream>
 #include <intrin.h>
 #include <iomanip>
 #include "HybridDetect.h"
+#include <chrono>
+#include <iostream>
+#include <thread>
+#include <stdint.h>
+#include <winreg.h>
+#include <tchar.h>
+
+
+
+
 
 //todo:rrw note frequency base and max issues
 #define LEAF_FREQUENCY_INFORMATION				0x16        // Processor Frequency Information Leaf  function 0x16 only works on Skylake or newer.
+#define MEM_DIV                                 1024        // Used for getting system memory information
 
 //todo:rrw do we want global?
 HybridDetect::PROCESSOR_INFO ProcessorInfo;
+typedef BOOL(WINAPI *PGetPhysicallyInstalledSystemMemory)(PULONGLONG TotalMemoryInKiloBytes);
+
+void GPUDriverVersion()
+{
+    std::cout << "\n\n GPU default or primary driver version details follow: " << std::endl;
+
+	// Fetch registry data
+	HKEY dxKeyHandle = nullptr;
+	DWORD numOfAdapters = 0;
+    unsigned int dxDriverVersion[4];
+
+	LSTATUS returnCode = ::RegOpenKeyEx( HKEY_LOCAL_MACHINE, _T("SOFTWARE\\Microsoft\\DirectX"), 0, KEY_READ, &dxKeyHandle );
+
+	if( returnCode != ERROR_SUCCESS )
+	{
+		std::cout << "Unable to open registry key handle" << std::endl;
+        return;
+	}
+
+	// Find all subkeys
+
+	DWORD subKeyMaxLength = 0;
+
+	returnCode = ::RegQueryInfoKey(
+		dxKeyHandle,
+		nullptr,
+		nullptr,
+		nullptr,
+		&numOfAdapters,
+		&subKeyMaxLength,
+		nullptr,
+		nullptr,
+		nullptr,
+		nullptr,
+		nullptr,
+		nullptr
+	);
+
+	if( returnCode != ERROR_SUCCESS )
+	{
+		std::cout << "Unable to read registry value" << std::endl;
+        return;
+	}
+
+	subKeyMaxLength += 1; // include the null character
+
+	uint64_t driverVersionRaw = 0;
+
+	bool foundSubkey = false;
+	TCHAR* subKeyName = new TCHAR[subKeyMaxLength];
+
+   //Code retrieves all the adaptors, typically the first adapter is the default one, use it to provide driver version
+	for( DWORD i = 0; i < 1; ++i )
+	{
+		DWORD subKeyLength = subKeyMaxLength;
+
+		returnCode = ::RegEnumKeyEx(
+			dxKeyHandle,
+			i,
+			subKeyName,
+			&subKeyLength,
+			nullptr,
+			nullptr,
+			nullptr,
+			nullptr
+		);
+
+		if( returnCode == ERROR_SUCCESS )
+		{
+			LUID adapterLUID = {};
+			DWORD qwordSize = sizeof( uint64_t );
+
+			returnCode = ::RegGetValue(
+				dxKeyHandle,
+				subKeyName,
+				_T("AdapterLuid"),
+				RRF_RT_QWORD,
+				nullptr,
+				&adapterLUID,
+				&qwordSize
+			);
+
+			if( returnCode == ERROR_SUCCESS) // If we were able to retrieve the registry values
+				 
+			{
+				// We have our registry key! Let's get the driver version num now
+
+				returnCode = ::RegGetValue(
+					dxKeyHandle,
+					subKeyName,
+					_T("DriverVersion"),
+					RRF_RT_QWORD,
+					nullptr,
+					&driverVersionRaw,
+					&qwordSize
+				);
+
+				if( returnCode == ERROR_SUCCESS )
+				{
+					foundSubkey = true;
+					break;
+				}
+			}
+		}
+	}
+
+	returnCode = ::RegCloseKey( dxKeyHandle );
+	assert( returnCode == ERROR_SUCCESS );
+	delete[] subKeyName;
+
+	if( !foundSubkey )
+	{
+		std::cout << "Error Missing Driver Info" << std::endl;
+        return;
+	}
+
+	// Now that we have our driver version as a DWORD, let's process that into something readable
+	dxDriverVersion[ 0 ] = (unsigned int) ( ( driverVersionRaw & 0xFFFF000000000000 ) >> 16 * 3 );
+	dxDriverVersion[ 1 ] = (unsigned int) ( ( driverVersionRaw & 0x0000FFFF00000000 ) >> 16 * 2 );
+	dxDriverVersion[ 2 ] = (unsigned int) ( ( driverVersionRaw & 0x00000000FFFF0000 ) >> 16 * 1 );
+	dxDriverVersion[ 3 ] = (unsigned int) ( ( driverVersionRaw & 0x000000000000FFFF ) );
+
+	std::cout << "\n "<< dxDriverVersion[0] << "." <<  dxDriverVersion[1] << "." <<  dxDriverVersion[2] << "." <<  dxDriverVersion[3]; 
+    std::cout << "\n" ; 
+
+}
+
+//todo: update to return version in production 
+void NPUDriverVersion()
+{
+    std::cout << "\n\n NPU driver version details follow: " << std::endl;
+    // Fetch registry data
+	HKEY nKeyHandle = nullptr;
+	DWORD numOfAdapters = 0;
+
+    //Start with getting a Key Handle 
+    LSTATUS returnCode = ::RegOpenKeyEx(HKEY_LOCAL_MACHINE, _T("SYSTEM\\CurrentControlSet\\Control\\Class\\{f01a9d53-3ff6-48d2-9f97-c8a7004be10c}"), 0, KEY_READ, &nKeyHandle );
+
+    if( returnCode != ERROR_SUCCESS )
+	{
+		std::cout << "Unable to open registry key handle" << std::endl;
+        return;
+	}
+    
+    //Get subkey from registry key, it is {0000} by default
+    DWORD subKeyMaxLength = 0;
+
+	returnCode = ::RegQueryInfoKey(
+		nKeyHandle,
+		nullptr,
+		nullptr,
+		nullptr,
+		&numOfAdapters,
+		&subKeyMaxLength,
+		nullptr,
+		nullptr,
+		nullptr,
+		nullptr,
+		nullptr,
+		nullptr
+	);
+    
+    if( returnCode != ERROR_SUCCESS )
+	{
+		std::cout << "Unable to enumerate registry subkey" << std::endl;
+        return;
+	}
+
+    subKeyMaxLength += 1; // include the null character
+
+	bool foundSubkey = false;
+	TCHAR* subKeyName = new TCHAR[subKeyMaxLength];
+
+    //Code will iterate through all adapters or subkeys, since we have a default value upfront setting this to single subkey for now
+	for( DWORD i = 0; i < 1; ++i )
+	{
+		DWORD subKeyLength = subKeyMaxLength;
+
+		returnCode = ::RegEnumKeyEx(
+			nKeyHandle,
+			i,
+			subKeyName,
+			&subKeyLength,
+			nullptr,
+			nullptr,
+			nullptr,
+			nullptr
+		);
+
+		if( returnCode == ERROR_SUCCESS )
+		{
+            DWORD dataSize = 0;
+            DWORD dataSize2 = 0;
+            DWORD dataSize3 = 0;
+            
+            
+    
+				// We have our registry key! Let's get the driver version num now
+                // Retrieve data size first so we can create a variable with the size needed 
+
+				returnCode = ::RegGetValue(
+					nKeyHandle,
+					subKeyName,
+					_T("DriverVersion"),
+					RRF_RT_REG_SZ,
+					nullptr,
+					nullptr,
+					&dataSize
+				);
+                
+                
+               
+                LPBYTE lpData = (LPBYTE)malloc(dataSize);
+
+                returnCode = ::RegGetValue(
+					nKeyHandle,
+					subKeyName,
+					_T("DriverVersion"),
+					RRF_RT_REG_SZ,
+					nullptr,
+                    lpData,
+					&dataSize
+				); 
+
+				if( returnCode == ERROR_SUCCESS )
+				{
+					foundSubkey = true;
+                    
+                    std::cout << "\n\n"; 
+                    printf("NPU Driver Version: %s\n", (char*)lpData);
+                 
+				}
+                free(lpData);
+
+                //Get NPU Driver Description 
+                returnCode = ::RegGetValue(
+					nKeyHandle,
+					subKeyName,
+					_T("DriverDesc"),
+					RRF_RT_REG_SZ,
+					nullptr,
+					nullptr,
+					&dataSize2
+				);
+                
+                
+               
+                LPBYTE lpData2 = (LPBYTE)malloc(dataSize2);
+
+                returnCode = ::RegGetValue(
+					nKeyHandle,
+					subKeyName,
+					_T("DriverDesc"),
+					RRF_RT_REG_SZ,
+					nullptr,
+                    lpData2,
+					&dataSize2
+				); 
+
+				if( returnCode == ERROR_SUCCESS )
+				{
+					foundSubkey = true;
+                    
+                    std::cout << "\n\n"; 
+                    printf("NPU Driver Description: %s\n", (char*)lpData2);
+                 
+				}
+                free(lpData2);
+
+                 //Get NPU Driver Inf Details - Includes NPU version
+                returnCode = ::RegGetValue(
+					nKeyHandle,
+					subKeyName,
+					_T("InfSection"),
+					RRF_RT_REG_SZ,
+					nullptr,
+					nullptr,
+					&dataSize3
+				);
+                
+                
+               
+                LPBYTE lpData3 = (LPBYTE)malloc(dataSize3);
+
+                returnCode = ::RegGetValue(
+					nKeyHandle,
+					subKeyName,
+					_T("InfSection"),
+					RRF_RT_REG_SZ,
+					nullptr,
+                    lpData3,
+					&dataSize3
+				); 
+
+				if( returnCode == ERROR_SUCCESS )
+				{
+					foundSubkey = true;
+                    
+                    std::cout << "\n\n"; 
+                    printf("NPU Driver Inf Description: %s\n", (char*)lpData3);
+                 
+				}
+                free(lpData3);
+			
+		}
+	}
+
+	returnCode = ::RegCloseKey( nKeyHandle );
+	assert( returnCode == ERROR_SUCCESS );
+	delete[] subKeyName;
+  
+
+	if( !foundSubkey )
+	{
+		std::cout << "Error Missing Driver Info" << std::endl;
+        return;
+	}
+
+
+}
 
 //todo:rrw comment out in production
 void OutputCoreType(int coreType) {
@@ -92,6 +424,7 @@ void UseHybridCPUInformation() {
     int coreType;
     int coreTypeIndex;
     int coreTypeCount[4];
+    
     HybridDetect::LOGICAL_PROCESSOR_INFO logicalCore;
 
     //todo:rrw comment out in production
@@ -131,6 +464,8 @@ void UseHybridCPUInformation() {
         std::cout << "\n\n Core Counts:\n";
         OutputTotalCoresOfCoreType(HybridDetect::INTEL_CORE, coreTypeCount[0]);
         OutputTotalCoresOfCoreType(HybridDetect::INTEL_ATOM, coreTypeCount[1]);
+
+        
     }
 }
 
@@ -138,33 +473,93 @@ void UseLegacyCPUInformation(std::string CPUBrandString) {
     std::cout << CPUBrandString << "\n";
 }
 
-//main
+void MemoryInformation()
+{
+    std::cout << " \n\n Memory Information follows....." << std::endl;
+    std::cout << "\n\n";
+
+    //Get information about memory installed on system
+    HINSTANCE hGetProcDLL = LoadLibrary("kernel32.dll");
+    if(!hGetProcDLL)
+    {
+        std::cout << "Unable to load dynamic library" << std::endl;
+        return;
+    }
+    ULONGLONG installedMem;
+	PGetPhysicallyInstalledSystemMemory getInstalledMem_out = (PGetPhysicallyInstalledSystemMemory)GetProcAddress(hGetProcDLL, "GetPhysicallyInstalledSystemMemory");
+	if (!getInstalledMem_out) {
+		std::cout << "Unable to locate the get installed memory function" << std::endl;
+		return;
+	}
+	
+	if (!getInstalledMem_out(&installedMem)) {
+		std::cout << "Error calling get installed memory function." << std::endl;
+
+		DWORD error = GetLastError();
+		std::cout << "Error code is: " << std::hex << error << std::endl;
+		return;
+	}
+	
+	std::cout << "total installed memory in GB: " << installedMem / MEM_DIV / MEM_DIV << std::endl;
+
+    //Get information about memory available on system
+    MEMORYSTATUSEX statusex;
+
+    statusex.dwLength = sizeof (statusex);
+
+    GlobalMemoryStatusEx (&statusex) ; 
+
+    std::cout << "Amount of memory in use in GB: " << (statusex.dwMemoryLoad * installedMem) / 100 / MEM_DIV / MEM_DIV << std::endl ; 
+
+    std::cout << "Amount of free physical memory available in KB: " << (statusex.ullAvailPhys) / MEM_DIV << std::endl; 
+
+    std::cout << "Amount of free physical paging file available in KB: " << (statusex.ullAvailPageFile) / MEM_DIV << std::endl;
+
+    std::cout << "Amount of free virtual memory available in KB: " << (statusex.ullAvailVirtual) / MEM_DIV << std::endl;
+
+
+
+}
+
+//main - uses Windows API only
+
 int main()
 {
-    int ret;
+    int ret = 0;
     bool bMTLorARL = false;
     int Max = 0; 
     int Base = 0;
+    int sleepTime = 2;
+    
 
     std::cout << "cpu detect!\n";
+   
 
     GetProcessorInfo(ProcessorInfo);
     if (ProcessorInfo.IsIntel() == true) {
 
         bMTLorARL = IsIntelMTLorARL();
         if (bMTLorARL) {
-            std::cout << "cpu is MTL or ARL!\n";
-            UseHybridCPUInformation();
+              std::cout << "cpu is MTL or ARL!\n";
+              UseHybridCPUInformation();
+              MemoryInformation(); //Get physical installed memory and memory availble
+              GPUDriverVersion(); // Get GPU driver version
+              NPUDriverVersion(); //Get NPU driver version
         }
         else {
             std::cout << "cpu is not MTL or ARL!\n";
             if (ProcessorInfo.hybrid == true) {
                 std::cout << "cpu is ADL!\n";
                 UseHybridCPUInformation();
+                MemoryInformation(); //Get physical installed memory and memory availble
+                GPUDriverVersion(); //Get GPU driver version
+
+        
             }
             else {
                 UseLegacyCPUInformation(ProcessorInfo.brandString);
-                UseHybridCPUInformation();
+                MemoryInformation(); //Get physical installed memory and memory availble
+                GPUDriverVersion(); //Get GPU driver version
             }
         }
     }
